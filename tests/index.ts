6e0f04bd5df180d7e09075e4eb87b489e454f8c3
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-28 16:15:15 +0100

    Test styles

--- tests/index.ts
+++ tests/index.ts
@@ -11,6 +11,20 @@ describe('react-highlight-updates', () => {
     expect(React.createElement).not.toBe(createElementSpy);
   });
 
+  it('should append styles to the head', () => {
+    const style = document.getElementsByTagName('style')[0];
+
+    expect(style.innerHTML.indexOf('rgba(255, 0, 0, 0.5)')).toBeGreaterThan(0);
+  });
+
+  it('should accept a custom color', () => {
+    highlightUpdates('rgba(0, 255, 0, 0.5)');
+
+    const style = document.getElementsByTagName('style')[0];
+
+    expect(style.innerHTML.indexOf('rgba(0, 255, 0, 0.5)')).toBeGreaterThan(0);
+  });
+
   it('should call the original createElement function', () => {
     const props = {foo: 'bar'};
     React.createElement('div', props, 'child1', 'child2');
